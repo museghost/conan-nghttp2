3905168f0c3f09dea5cc36b07fbfacbb2aa05850
brian.kim <brian.kim@beatthemarket.co.kr>
2019-03-16 20:21:12 +0900

    changed script

--- conanfile.py
+++ conanfile.py
@@ -4,11 +4,6 @@
 from conans import ConanFile, CMake, tools
 import os
 
-
-kOPTIONS = ("with_asio", "with_libxml2", "with_jemalloc",
-            "with_spdylay", "with_libevent",
-            "with_jansson", "with_libcares", "with_libev")
-
 _FIND_PACKAGES = {'with_libxml2': 'find_package(LibXml2 2.6.26)',
                   'with_jemalloc': 'find_package(Jemalloc)',
                   'with_spdylay': 'find_package(Spdylay 1.3.2)',
@@ -19,24 +14,46 @@ _FIND_PACKAGES = {'with_libxml2': 'find_package(LibXml2 2.6.26)',
 
 class Nghttp2Conan(ConanFile):
     name = "nghttp2"
-    version = "1.36.0"
+    version = "1.37.0"
     url = "https://github.com/museghost/nghttp2"
     description = "HTTP/2 C Library and tools"
     license = "https://raw.githubusercontent.com/nghttp2/nghttp2/master/LICENSE"
     settings = "os", "arch", "compiler", "build_type"
-    options = {option: [True, False] for option in kOPTIONS}
-    default_options = tuple("{0}=False".format(option) for option in options)
+    options = {"shared": [True, False],
+               "fPIC": [True, False],
+               "with_asio": [True, False],
+               "with_libxml2": [True, False],
+               "with_jemalloc": [True, False],
+               "with_spdylay": [True, False],
+               "with_libevent": [True, False],
+               "with_jansson": [True, False],
+               "with_libcares": [True, False],
+               "with_libev": [True, False]}
+    default_options = ("shared=False",
+                       "fPIC=True",
+                       "with_asio=False",
+                       "with_libxml2=False",
+                       "with_jemalloc=False",
+                       "with_spdylay=False",
+                       "with_libevent=False",
+                       "with_jansson=False",
+                       "with_libcares=False",
+                       "with_libev=False")                       
     exports_sources = ['CMakeLists.txt']
     generators = 'cmake'
 
     def requirements(self):
-        self.requires.add("OpenSSL/1.1.0j@conan/stable")
+        self.requires.add("OpenSSL/1.0.2r@conan/stable")
 
         if self.options.with_asio:
             self.requires.add("OpenSSL/[>1.0.2a,<1.0.3]@conan/stable")
 
             for boost_lib in ('Asio', 'System', 'Thread'):
                 self.requires.add("Boost.{0}/1.65.1@bincrafters/stable".format(boost_lib))
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            self.options.remove("fPIC")
 
     def source(self):
         source_url = "https://github.com/museghost/nghttp2.git"
@@ -47,15 +64,23 @@ class Nghttp2Conan(ConanFile):
         #os.rename(extracted_dir, "sources")
 
     def build(self):
+        # git pull
+        self.run("cd sources && git pull")
+
         cmake = CMake(self)
         cmake.definitions["ENABLE_LIB_ONLY"] = True
         cmake.definitions["CMAKE_INSTALL_PREFIX"] = self.package_folder
 
-        #if self.options.shared:
-        cmake.definitions['ENABLE_SHARED_LIB'] = True
-        cmake.definitions['ENABLE_STATIC_LIB'] = True
-        cmake.definitions['BUILD_SHARED_LIBS'] = True
-        cmake.definitions['BUILD_STATIC_LIBS'] = True
+        if self.options.shared:
+            cmake.definitions['ENABLE_SHARED_LIB'] = "ON"
+            cmake.definitions['ENABLE_STATIC_LIB'] = "OFF"
+            cmake.definitions['BUILD_SHARED_LIBS'] = "ON"
+            cmake.definitions['BUILD_STATIC_LIBS'] = "OFF"
+        else:
+            cmake.definitions['ENABLE_SHARED_LIB'] = "OFF"
+            cmake.definitions['ENABLE_STATIC_LIB'] = "ON"
+            cmake.definitions['BUILD_SHARED_LIBS'] = "OFF"
+            cmake.definitions['BUILD_STATIC_LIBS'] = "ON"
 
         # Use dependency version of openssl
         openssl_root_dir = self.deps_cpp_info["OpenSSL"].rootpath
@@ -82,6 +107,9 @@ class Nghttp2Conan(ConanFile):
                 'sources/CMakeLists.txt',
                 'find_package(Boost 1.54.0 REQUIRED system thread)',
                 '')
+
+        if self.settings.compiler != 'Visual Studio':
+            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
 
         cmake.configure()
         cmake.build()
